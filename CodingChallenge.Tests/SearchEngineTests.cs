1e6d683bfd1c12fdfe5e7bfcc4fc2de432b80c91
Zia <mzhaqch@yahoo.com>
2021-05-24 16:29:12 +0100

    Add project files.

---
+++ CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+
+        [Test]
+        public void SearchForNonExistentShirtTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                  new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.White},
+                Sizes = new List<Size> { Size.Large }
+
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.IsTrue(results.Shirts.Count == 0); //Shirts count should be zero as no 'white - large' in the store
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void SearchWithNoOptionFilterTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                  new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.IsTrue(results.Shirts.Count == 5); //Specifying no filter should return all the available shirts
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void SearchAgainstSizeOnlyOptionTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                  new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Sizes = new List<Size> { Size.Large }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.IsTrue(results.Shirts.Count == 2); //should return 2 in total as only two large shirts available
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+        [Test]
+        public void SearchAgainstColorOnlyOptionTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                 new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                  new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { Color.Red }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.IsTrue(results.Shirts.Count == 3); //should return 3 items in total as only three red shirts available
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+      
+        [Test]
+        public void SearchAgainstBothColorAndSizeOptionTest()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+                new Shirt(Guid.NewGuid(), "Red - Large", Size.Large, Color.Red),
+                  new Shirt(Guid.NewGuid(), "Red - Medium", Size.Medium, Color.Red),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red, Color.Blue},
+                Sizes = new List<Size> {Size.Large}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            //should return 2 items in total as only two red shirts available in Red and Blue color with Large size
+            
+            Assert.IsTrue(results.Shirts.Count == 2);
+                       
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+    }
+}
---
+++ CodingChallenge/SearchEngine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+        private readonly Dictionary<(Guid color, Guid size), List<Shirt>> _colorSizeList;
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+            //Our search is based on color and sizes, we can build a dictionary on these two options for quick retrieval of results
+            _colorSizeList = shirts?.GroupBy(s => (s.Color.Id, s.Size.Id)).ToDictionary(grp => grp.Key, grp => grp.ToList());
+        }
+
+        /// <summary>
+        /// Builds all possible keys/combinations for passed in search options, it will create keys based on color and size filter
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        private IEnumerable<(Guid color, Guid size)> GetSearchKeys(SearchOptions options)
+        {
+            var colors = options?.Colors?.Count > 0 ? options.Colors : Color.All;
+            var sizes = options?.Sizes?.Count > 0 ? options.Sizes : Size.All;
+
+            return from c in colors
+                   from s in sizes
+                   select (c.Id, s.Id);
+
+
+        }
+
+        public SearchResults Search(SearchOptions options)
+        {
+            var lstShirts = new List<Shirt>();
+            var colorCounts = Color.All.Select(c => new ColorCount() { Color = c, Count = 0 }).ToList();
+            var sizeCounts = Size.All.Select(s => new SizeCount() { Size = s, Count = 0 }).ToList();
+
+
+            //against the search color and/or size options get the matching shirts and update the color and size counters
+            foreach (var key in GetSearchKeys(options))
+            {
+                if (_colorSizeList.TryGetValue(key, out List<Shirt> shirts))
+                {
+                    lstShirts.AddRange(shirts);
+                    var colorCount = colorCounts.First(i => i.Color.Id == key.color);
+                    colorCount.Count += shirts.Count;
+                    var sizeCount = sizeCounts.First(i => i.Size.Id == key.size);
+                    sizeCount.Count += shirts.Count;
+                }
+            }
+
+            return new SearchResults
+            {
+                ColorCounts = colorCounts,
+                Shirts = lstShirts,
+                SizeCounts = sizeCounts
+            };
+        }
+    }
+}
